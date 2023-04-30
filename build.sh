41f57eb9482d82e81637c3ed55ea279b0786c6f2
VukAmidzic <vuk.amidzic@gmail.com>
2023-04-30 14:14:14 +0200

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,35 +1,39 @@
 #!/bin/bash
 
-lex_res= flex --version > /dev/null 
-if [[ $lex_res -ne 0 ]]; then
+flex --version 2>/dev/null 
+if [[ $? != 0 ]]; then
     sudo apt install flex
     echo "[INFO] Flex installed"
 fi
 
-bison_res= bison --version > /dev/null
-if [[ $bison_res -ne 0 ]]; then
+bison --version 2>/dev/null
+if [[ $? != 0 ]]; then
     sudo apt install bison
     echo "[INFO] Bison installed"
 fi
 
 lex frontend/lexer.l
 echo "[INFO] Lexer successfully built"
-bison -d frontend/parser.ypp 2>/dev/null || (exit 0)
+bison -d frontend/parser.ypp 2>/dev/null
 g++ -Wall -Wextra ast/ast.cpp lex.yy.c parser.tab.cpp -g -o exp
 echo "[INFO] Parser successfully built"
 
 exp () {
     if [[ "$1" == *.exp ]]; then
         ./exp $1 > "${2}.s"
-        echo "[INFO] AST tree successfully created"
+        if [[ $? -eq 0 ]]; then
+            echo "[INFO] AST tree successfully created"
+        else 
+            echo "[ERROR] Error while creating AST tree"
+        fi
     else 
         echo "[ERROR] Wrong file extension; expected .exp at the end..."
     fi 
     
-    if [ -s "${2}.s" ];
+    if [[ -s "${2}.s" ]];
     then 
-        comp_res= gcc asm/asm_ops.c -m64 -fno-pie -no-pie "${2}.s" -g -o $2 2>/dev/null
-        if [[ $comp_res -ne 0 ]]; then
+        gcc asm/asm_ops.c -m64 -fno-pie -no-pie "${2}.s" -g -o $2 2>/dev/null
+        if [[ $? -ne 0 ]]; then
             echo "[ERROR] Compilation error"
         else 
             echo "[INFO] Successfull compilation"
